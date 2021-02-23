09fd108cdeac7c5bc02fd4d04cfc58b6f51b291e
Ting KC <ting.kc.56@gmail.com>
2021-02-23 16:37:44 +0100

    '2'

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+echo "hello";
+
+require('vendor/autoload.php');
+// try{
+//    $db = new PDO('mysql:host=localhost:3306;dbname=supercoffee', 'root', '');
+//     echo "connected";
+// }   catch (PDOException $e){
+//     echo "not connected error:" .$e->getMessage();
+// }
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$path="mysql:host=".$_ENV['DB_DNS'].";dbname=".$_ENV['DB_NAME'].";port=".$_ENV['DB_port'];
+try{
+   $db = new PDO($path,$_ENV['DB_USER'],$_ENV['DB_PASS']);
+ //$db = new PDO('mysql:host=mysql-21126-0.cloudclusters.net;dbname=superCoffee;port=21126', 'adminTing', 'adminTing');
+    echo "connected";
+}   catch (PDOException $e){
+    echo "not connected error:" .$e->getMessage();
+}
+
+$jojos = $db->query('SELECT * from waiter');
+
+foreach($jojos as $jojo){
+    echo $jojo['name'];
+}
+
+
+$sqlRec = 'SELECT w.name as name, format(sum(price),2) as turnover  
+                    FROM orders as o
+                    inner join ordersfood as of on o.id=of.idOrders
+                    inner join fooditem as f on f.id=of.idFood
+                    inner join waiter as w on o.idWaiter = w.id
+                    group by w.id ';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>superCoffee</title>
+</head>
+<body>
+<main>
+<table>
+<thead>
+    <tr>
+        <th>NOM</th>
+        <th>CA</th>
+    </tr>
+</thead>
+<tbody>
+<?php
+         foreach($db->query($sqlRec) as $waiter){
+             echo "<tr>";
+         echo "<td>".$waiter['name']."</td>";
+         echo "<td>".$waiter['turnover']."</td>";  
+         echo "</tr>";   
+         };
+         ?> 
+</tbody>
+</table>
+    
+</main>
+    
+</body>
+</html>
--- super_coffee.php
+++ super_coffee.php
@@ -1,3 +1,17 @@
 <?php
-echo 'hello world';
+echo 'hello world test';
+
+try{
+    $conn = new PDO('mysql:host=127.0.0.1:8888; dbname=supercoffee', 'root', '');
+
+    echo "连接成功";
+}catch (PDOException $e){
+    echo $e->getMessage();
+}finally{
+    $conn = null;
+    echo "关闭数据库连接";
+}
+
+// $waiters = $db->query('SELECT * from waiter')->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
